581376fdac9df75b4fb21ad42dc5fd0dc4f3a33e
Ferysuryanta <ferysuryanta01@gmail.com>
2023-10-27 09:48:49 +0700

    backend_test

---
+++ src/main/java/com/backend_test/BackendTestFeryApplication.java
@@ -0,0 +1,13 @@
+package com.backend_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestFeryApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run ( BackendTestFeryApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/backend_test/BackendTestFeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestFeryApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
